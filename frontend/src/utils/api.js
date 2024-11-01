295ec7d92141dd5e936281a2dfb44c36b9929821
Nathan Useni <164697947+omormehn@users.noreply.github.com>
2024-11-01 01:41:22 +0100

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.SERVER_SIDE}/api`,
+  baseURL: https://estate-web-backend.onrender.com/api`,
   withCredentials: true,
 });
 
