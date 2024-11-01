a56fab1a17e42aa80a1a653ad428af40c29b90a3
Nathan Useni <164697947+omormehn@users.noreply.github.com>
2024-11-01 02:00:52 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000; // it can take 3000 as a fallback
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://estate-fullstack.netlify.app",
     credentials: true
 }))
 
