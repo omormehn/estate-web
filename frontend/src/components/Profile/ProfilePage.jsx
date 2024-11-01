e74aa6c4fa45f86d4e4d54a1f412baf03ddd5f0a
Nathan Useni <164697947+omormehn@users.noreply.github.com>
2024-11-01 01:03:52 +0100

    Update ProfilePage.jsx

--- frontend/src/components/Profile/ProfilePage.jsx
+++ frontend/src/components/Profile/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import { Button } from "@material-tailwind/react";
-import Chat from "../../components/chat/Chat";
+import Chat from "../Chat/Chat";
 import "./profile.scss"
 import {api} from "../../utils/api";
 import { useNavigate } from "react-router-dom";
