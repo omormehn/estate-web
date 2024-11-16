5f3b9610688b279ea3dff42d735fc5740b9e27a4
Nathan Useni <164697947+omormehn@users.noreply.github.com>
2024-11-16 05:03:10 +0100

    Update generateTokenAndSetCookie.js

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -6,8 +6,8 @@ export const generateTokenAndSetCookie = (res, userId) => {
     });  
         res.cookie("token", token, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: "strict",
+          secure: true,
+          sameSite: "None",
           maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
         });
         return token;
