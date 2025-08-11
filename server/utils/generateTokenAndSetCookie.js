4059c45e65ceb8cfac4c126485b5b736ef19c48b
omormehn <nathanuseni@gmail.com>
2025-08-11 04:48:50 +0100

    fixed cors

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -1,16 +1,14 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const generateTokenAndSetCookie = (res, userId) => {
-    const token = jwt.sign({ userId, isAdmin:false }, process.env.JWT_SECRET, {
-      expiresIn: "1hr",
-    });  
-        res.cookie("token", token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: "Strict",
-          maxAge: 3600000,
-        });
-        return token;
-}
-
-            
+  const token = jwt.sign({ userId, isAdmin: false }, process.env.JWT_SECRET, {
+    expiresIn: "1hr",
+  });
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    maxAge: 3600000,
+  });
+  return token;
+};
